8a7acfa390fa68b0d625f0b570c2637d41efddfe
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2021-09-27 19:59:33 +0700

    first commit

---
+++ src/sendfile/Entry.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sendfile;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Entry {
+    public static void main(String[] args) {
+        //Action action = new Action();
+        Action action = new Action();
+        action.getGui().setVisible(true);
+    }
+}
